cebdef0dd0e027b4700647d669a5773babad7ebd
ceva9529 <59778360+ceva9529@users.noreply.github.com>
2020-03-19 00:01:56 -0600

    Add files via upload

---
+++ procesar.php
@@ -0,0 +1,18 @@
+
+<link rel="stylesheet" href="css/style.css">    
+<?php
+
+$nombreAlumno = $_POST['nombre'];
+$apellidosAlumno = $_POST['apellidos'];
+
+echo 'tu nombre es:'.'&nbsp'.$nombreAlumno.'&nbsp'. $apellidosAlumno;
+
+
+
+/* NOMBRE DEL ALUMNO: CESAR EDWIN VAZQUEZ AGUILAR - LSC - GRADO: 5TO - GRUPO: J */
+?>
+
+
+
+
+
